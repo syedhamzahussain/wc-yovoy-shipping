294da82c7362b5d791d2e8c1e8c228c7c5829e7f
Syed Hamza Hussain <40966603+syedhamzahussain@users.noreply.github.com>
2021-06-03 20:24:06 +0500

    version update

--- wc-yovoy-shipping.php
+++ wc-yovoy-shipping.php
@@ -3,7 +3,7 @@
  * Plugin Name: YoVoy
  * Description: Woocommerce YoVoy Shipping Integration.
  * Author: Salebien LLC
- * Version: 1.1.1.27
+ * Version: 1.1.1.28
  * Text Domain: wcys
  * Domain Path: /languages
  * Author URI: #
